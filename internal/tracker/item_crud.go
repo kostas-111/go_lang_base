9a553aa07029b20893ee65f454aaa42a7338cf44
Konstantin Galkin <galkink370@gmail.com>
2026-09-08 22:27:54 +0300

    feat: Инкапсуляция [#505565 #567013] - Поправь код так, чтобы избежать утечки внутреннего состояния структуры Tracker

---
+++ internal/tracker/item_crud.go
@@ -0,0 +1,21 @@
+package tracker
+
+
+
+type Tracker struct {
+  items []Item
+}
+
+func NewTracker() *Tracker {
+  return &Tracker{}
+}
+
+func (t *Tracker) AddItem(item Item) {
+  t.items = append(t.items, item)
+}
+
+func (t *Tracker) GetItems() []Item {
+  res := make([]Item, len(t.items))
+  copy(res, t.items)
+  return res
+}
---
+++ internal/tracker/item_crud_test.go
@@ -0,0 +1,40 @@
+package tracker
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTrackerGetItems(t *testing.T) {
+  t.Parallel()
+  t.Run("check link leak", func(t *testing.T) {
+      t.Parallel()
+
+      tracker := NewTracker()
+      item := Item{
+          ID:   "1",
+          Name: "First Item",
+      }
+      tracker.AddItem(item)
+
+      res := tracker.GetItems()
+      res[0].Name = "Second Item"
+
+      assert.Equal(t,
+          []Item{item},
+          tracker.GetItems(),
+      )
+  })
+}
+
+func TestTrackerAddOne(t *testing.T) {
+    t.Parallel()
+    tracker := NewTracker()
+    item := Item{ID: "1", Name: "First"}
+    tracker.AddItem(item)
+    items := tracker.GetItems()
+    assert.Len(t, items, 1)
+    assert.Equal(t, item, items[0])
+}
+
